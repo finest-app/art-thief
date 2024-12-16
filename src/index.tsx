ed83accea896afbe0a4a1ac110c94e13712e91a8
Visualizeit <vnyeit@gmail.com>
2024-12-16 16:22:12 +0800

    replace valibot-validator with zod-openapi and update API routes for quote card generation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,45 @@
-import { vValidator } from '@hono/valibot-validator'
+import { createRoute, z, OpenAPIHono } from '@hono/zod-openapi'
 import { Resvg } from '@resvg/resvg-js'
-import { Hono } from 'hono'
-import { object, string } from 'valibot'
+import { apiReference } from '@scalar/hono-api-reference'
 import getQuoteCard from './getQuoteCard'
 import renderer from './renderer'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
-app.use(renderer)
+if (false) {
+	app.use(renderer)
+}
 
-app.get(
-	'/',
-	vValidator('query', object({ quote: string(), author: string() })),
+app.openapi(
+	createRoute({
+		path: '/image',
+		method: 'get',
+		request: {
+			query: z.object({
+				quote: z.string().openapi({
+					description: 'Quote to render',
+					example: '如果你只读别人都在读的书，你就只能想别人所想。',
+				}),
+				author: z.string().openapi({
+					description: 'Author of the quote',
+					example: '村上春树',
+				}),
+			}),
+		},
+		responses: {
+			200: {
+				description: 'Image of quote card',
+				content: {
+					'image/png': {
+						schema: {
+							type: 'string',
+							format: 'binary',
+						},
+					},
+				},
+			},
+		},
+	}),
 	async (c) => {
 		const { quote, author } = c.req.valid('query')
 
@@ -27,6 +55,26 @@ app.get(
 			},
 		})
 	},
+)
+
+app.doc31('/openapi.json', {
+	openapi: '3.1.0',
+	info: {
+		title: 'Quote Card API',
+		description: 'API for generating quote cards',
+		version: '1',
+	},
+})
+
+app.get(
+	'/',
+	apiReference({
+		theme: 'deepSpace',
+		favicon: '/favicon.png',
+		spec: {
+			url: '/openapi.json',
+		},
+	}),
 )
 
 export default app
