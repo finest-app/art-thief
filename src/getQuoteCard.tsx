2140797541acaa09adf357513e4d3d837af90661
Visualizeit <vnyeit@gmail.com>
2024-12-16 15:10:54 +0800

    add getQuoteCard function and replace quote rendering logic

---
+++ src/getQuoteCard.tsx
@@ -0,0 +1,51 @@
+import satori from 'satori'
+
+type ReactNode = Parameters<typeof satori>[0]
+
+const getQuoteCard = async (content: string, author: string) => {
+	const fontFileResponse = await fetch(
+		'https://github.com/lxgw/LxgwWenKai-Lite/raw/refs/heads/main/fonts/TTF/LXGWWenKaiMonoLite-Light.ttf',
+	).then((response) => response.arrayBuffer())
+
+	const quoteCard = await satori(
+		(
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					width: '100%',
+					height: '100%',
+					backgroundColor: '#f8f9fa',
+					padding: 20,
+				}}
+			>
+				<p style={{ fontSize: 72 }}>”</p>
+				<p
+					style={{
+						fontSize: 24,
+						textWrap: 'balance',
+					}}
+				>
+					{content}
+				</p>
+				<p style={{ fontSize: 18, color: '#6c757d' }}>— {author}</p>
+			</div>
+		) as ReactNode,
+		{
+			width: 600,
+			height: 400,
+			fonts: [
+				{
+					name: 'LXGW WenKai Mono Lite Light',
+					data: fontFileResponse,
+					weight: 400,
+					style: 'normal',
+				},
+			],
+		},
+	)
+
+	return quoteCard
+}
+
+export default getQuoteCard
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import { Resvg } from '@resvg/resvg-js'
 import { Hono } from 'hono'
-import quote from './quote'
+import getQuoteCard from './getQuoteCard'
 import renderer from './renderer'
 
 const app = new Hono()
 
 app.use(renderer)
 
+const quotes = [
+	{
+		content: '如果你只读别人都在读的书，你就只能想别人所想。',
+		author: '村上春树',
+	},
+	{
+		content: '阅读一本书有两个动机：一是你喜欢这本书；二是你可以夸耀这本书。',
+		author: '伯特兰·罗素',
+	},
+]
+
 app.get('/', async (c) => {
-	const resvg = new Resvg(quote, {
+	const quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+	const quoteCard = await getQuoteCard(quote.content, quote.author)
+
+	const resvg = new Resvg(quoteCard, {
 		fitTo: { mode: 'original' },
 		font: {
 			loadSystemFonts: true,
