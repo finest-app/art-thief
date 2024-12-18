c8aebb773402c458b7fb1036172d1c4deeb4bed9
Visualizeit <vnyeit@gmail.com>
2024-12-18 18:21:39 +0800

    add vite-plugin-static-copy for WASM asset handling and update Miniflare configuration

--- miniflare.js
+++ miniflare.js
@@ -2,6 +2,7 @@ import { Miniflare, Log, LogLevel } from 'miniflare'
 
 new Miniflare({
 	modules: true,
+	modulesRules: [{ type: 'CompiledWasm', include: ['**/*.wasm'] }],
 	compatibilityDate: '2024-12-05',
 	scriptPath: './dist/_worker.js',
 	log: new Log(LogLevel.DEBUG),
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,45 @@ import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+	plugins: [
+		build({ emptyOutDir: true }),
+		devServer({
+			adapter,
+			entry: 'src/index.tsx',
+		}),
+		viteStaticCopy({
+			targets: [
+				{
+					src: [
+						'node_modules/@vercel/og/dist/resvg.wasm',
+						'node_modules/@vercel/og/dist/yoga.wasm',
+					],
+					dest: '',
+				},
+			],
+		}),
+		{
+			name: 'wasm-normalizer',
+			config: () => ({
+				build: { rollupOptions: { external: /\.wasm\?module$/ } },
+			}),
+			renderChunk: (code, renderedChunk) => {
+				let result = code
+
+				renderedChunk.imports.forEach((entry) => {
+					if (entry.endsWith('.wasm?module')) {
+						result = result.replace(
+							entry,
+							entry.replace(/.*\/([^\/]+\.wasm)\?.*/, '$1'),
+						)
+					}
+				})
+
+				return result
+			},
+		},
+	],
 })
